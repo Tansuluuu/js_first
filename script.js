0f70565a0543347a74e9d1fe61afb2552980ee13
Tansuluuu <tynalievatan@gmail.com>
2023-07-10 12:37:14 +0600

    js data types

---
+++ script.js
@@ -0,0 +1,38 @@
+ // let a=4
+ // let b=55
+ //
+ // console.log(a + b)
+
+ // let a=4
+ // let b=55
+ //
+ // console.log(a * b)
+
+ // let a=4
+ // let b=55
+ //
+ // console.log(a / b)
+
+ // let a=4
+ // let b=55
+ // console.log(a % b)
+
+ // let a ='hello'
+ // let b ='Гульчапчак'
+ //
+ // console.log(a + ' '+b)
+
+ // let a=45
+ // let b='lol'
+ //
+ // console.log(a / b)
+
+ // let a=5
+ // let b='5'
+ //
+ // console.log(a + + b)
+
+ // let c=2
+ // let b=undefined
+ //
+ // console.log(b)
